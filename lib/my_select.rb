cc44764b308116205923544afdde884a01e27b5b
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-15 19:11:51 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  newArr = []
+  while i < collection.length do
+    if yield(collection[i])
+      newArr << collection[i]
+    end
+    i += 1
+  end
+  newArr
 end
