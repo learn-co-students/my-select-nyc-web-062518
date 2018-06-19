66f01a2fb6c9caa0bb8897a4414d69aa0c0e5106
Daniel Ran <danielr1735@gmail.com>
2018-06-19 21:22:07 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  selected = []
+  i = 0
+  while i < collection.length
+    current = yield(collection[i])
+    if current == true
+      selected.push(collection[i])
+    end
+    i += 1
+  end
+  selected
 end
