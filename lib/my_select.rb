001e6c0444dceb3891be8fd17ab0e6b4716bd0e5
Jack Yeung <jyeung.se@gmail.com>
2018-06-19 17:45:00 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
- # your code here!
+  new_collection = []
+  i = 0
+  if collection.length > 0
+    while i < collection.length
+      if collection[i].even? == true
+        yield collection[i]
+        new_collection << collection[i]
+        i += 1
+      else
+        i += 1
+      end
+    end
+  else
+    return []
+  end
+  new_collection
 end
