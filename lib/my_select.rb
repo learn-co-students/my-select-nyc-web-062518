dee1eb1c9f79173804e9a85e261d874e122b512f
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 02:00:15 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
+###creating new array containing all elements of collection for which block condition is true
+
+
 def my_select(collection)
- # your code here!
+  i=0
+  a=Array.new
+  while i<collection.size
+    bool = yield collection[i]
+    if bool
+      a << collection[i]
+    end
+    i+=1
+  end
+  a
 end
